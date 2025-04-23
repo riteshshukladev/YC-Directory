0843b1693fb8b57b29b2d4664bedaa39be9c09f5
RItesh <shuklaritesh5678@gmail.com>
2025-04-23 23:37:04 +0530

    hero section done along with custom utilities for the styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
-
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+export default async function Home({searchParams}: {searchParams: {query: string}}) {
+  const query = (await searchParams).query || "";
   return (
     <>
-      Home
+      <section className="pink_container pattern">
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with entrepreneurs
+        </h1>
+        <p className="sub-heading">
+          Join us to explore new opportunities and grow your network.
+        </p>
+        <SearchForm query={query}/>
+      </section>
     </>
   );
 }
